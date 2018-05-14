80164b8cce246e46040a6617c67eef1471d12d2a
Diego Alfaro <diegoalfa98@gmail.com>
2018-05-14 12:33:22 +0200

    Fixed Scroll Issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import {TabsPage} from "../pages/tabs/tabs";
-import {VanillaPage} from "../pages/vanilla/vanilla";
 import {MapfrecitoComponent} from "../pages/mapfrecito/mapfrecito.component";
 import {MessageComponent} from "../components/message/message";
 
@@ -18,14 +14,13 @@ import {MessageComponent} from "../components/message/message";
 import { MapfreService } from '../services/mapfre.service';
 
 import { HttpClientModule } from '@angular/common/http';
+import {HomePage} from "../pages/home/home";
 
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    TabsPage,
-    VanillaPage,
     MapfrecitoComponent,
     MessageComponent
 
@@ -44,8 +39,6 @@ import { HttpClientModule } from '@angular/common/http';
   entryComponents: [
     MyApp,
     HomePage,
-    TabsPage,
-    VanillaPage,
     MapfrecitoComponent,
     MessageComponent
   ],
--- src/pages/mapfrecito/mapfrecito.component.ts
+++ src/pages/mapfrecito/mapfrecito.component.ts
@@ -1,90 +1,69 @@
-import {Component, OnInit, ViewChild} from "@angular/core";
-import {Message} from "../../app/classes/Message";
-import {Content} from "ionic-angular";
-import {MapfreService} from "../../services/mapfre.service";
-
-
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Message } from "../../app/classes/Message";
+import { Content, Grid } from "ionic-angular";
+import { MapfreService } from "../../services/mapfre.service";
 
 @Component({
   selector: 'page-mapfrecito',
-  templateUrl: 'mapfrecito.template.html',
-
-
-
-
+  templateUrl: 'mapfrecito.template.html'
 })
-export class MapfrecitoComponent implements OnInit{
+export class MapfrecitoComponent implements OnInit {
 
-  @ViewChild(Content) content:Content;
+  @ViewChild(Content) content: Content;
+  @ViewChild(Grid, { read: ElementRef }) messageFeedNode: ElementRef;
 
-  public messageFeed:Message[];
+  public messageFeed: Message[];
   public lastMsg: string;
+  private messageFeedChangeObserver: MutationObserver;
 
-  constructor(private mapfreService: MapfreService){
+  constructor(private mapfreService: MapfreService) {
 
-
-    this.messageFeed = [
-
-
-
-    ];
+    this.messageFeed = [];
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
 
-
-    this.mapfreService.sendQuery('Hola').subscribe((result:any) =>{
-
-
-      this.messageFeed.push(new Message(result.result.speech,'bot'));
-
-
+    this.mapfreService.sendQuery('Hola').subscribe((result: any) => {
+      this.messageFeed.push(new Message(result.result.speech, 'bot'));
     });
 
+    this.messageFeedChangeObserver = new MutationObserver((mutations) => {
+      this.scrollToBottom()
+    });
 
-
+    this.messageFeedChangeObserver.observe(this.messageFeedNode.nativeElement, {
+      childList: true
+    });
   }
 
 
   /**
    * This method uses de template-binded variable 'lastMsg' in order to send query to DialogFlow API
    */
-  public enviarMensaje(){
+  public enviarMensaje() {
 
-    this.scrollToBottom();
-
-    if(this.lastMsg && this.lastMsg != '') {
-
+    if (this.lastMsg && this.lastMsg != '') {
 
       this.messageFeed.push(new Message(this.lastMsg, 'user'));
 
       this.mapfreService.sendQuery(this.lastMsg).subscribe((result: any) => {
 
-
         this.messageFeed.push(new Message(result.result.speech, 'bot'));
-
 
       });
 
       this.lastMsg = '';
-
     }
+  }
 
-
-
+  public cargarMensajes(infiniteScrollEvent) {
 
   }
-  public cargarMensajes(infiniteScrollEvent){
 
+  public scrollToBottom() {
 
-
-
-
-
-  }
-  public scrollToBottom(){
     this.content.scrollToBottom(300);
-  }
 
+  }
 }

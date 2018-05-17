52ea6a48acdf0dc170717ade9f7cff903e4bb121
Diego Alfaro <diegoalfa98@gmail.com>
2018-05-17 16:47:25 +0200

    Video functionality gone, added interface 'Insured'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,27 @@
-
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
-
-//PAGES
-import { LoginPage } from '../pages/login/login';
-import { MapfrecitoComponent } from "../pages/mapfrecito/mapfrecito.component";
-
-
-
-
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
 //Modules
-import {Toast} from '@ionic-native/toast'
+import {IonicApp, IonicErrorHandler, IonicModule, ToastController} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+//PAGES
+import {LoginPage} from '../pages/login/login';
+import {MapfrecitoComponent} from "../pages/mapfrecito/mapfrecito.component";
 import {Camera} from "@ionic-native/camera";
 import {MediaCapture} from "@ionic-native/media-capture";
-import { HttpClientModule } from '@angular/common/http';
-import { Media } from '@ionic-native/media';
-import { File } from '@ionic-native/file';
-import { NativeAudio } from "@ionic-native/native-audio"
-
+import {HttpClientModule} from '@angular/common/http';
+import {Media} from '@ionic-native/media';
+import {File} from '@ionic-native/file';
 //SERVICES
-import { MapfreService } from '../services/mapfre.service';
+import {MapfreService} from '../services/mapfre.service';
 //Components
-import { MyApp } from './app.component';
+import {MyApp} from './app.component';
 import {MessageAbstract} from "../components/message/message_abstract.component";
 import {MessageTextComponent} from "../components/message/message_text/message_text.component";
 import {MessageCameraIntentComponent} from "../components/message/message_photoIntent/message_cameraIntent.component";
-import { MessageAudioIntentComponent } from '../components/message/message_audioIntent/message_audioIntent.component';
-import { CameraMock } from '../services/mocks/camera.mock';
-import { SanitizerPipe } from '../pipes/sanitizer.pipe';
-
+import {MessageAudioIntentComponent} from '../components/message/message_audioIntent/message_audioIntent.component';
+import {CameraMock} from '../services/mocks/camera.mock';
+import {SanitizerPipe} from '../pipes/sanitizer.pipe';
 
 
 @NgModule({
@@ -74,7 +63,7 @@ import { SanitizerPipe } from '../pipes/sanitizer.pipe';
     StatusBar,
     SplashScreen,
    Camera,CameraMock,
-   Toast,
+   ToastController,
     MapfreService,
     MediaCapture,
     Media,
